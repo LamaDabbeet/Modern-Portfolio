ebe77d5e12ba552ba80773bfec3013713753448f
LamaDabbeet <lamadabbeet@gmail.com>
2018-10-10 00:38:45 +0300

    Menu Overlay

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,6 +1,6 @@
 // Select DOM Elements
 const menuBtn = document.querySelector(".menu-btn");
-const navBranding = document.querySelector(".nav-branding");
+const menuBranding = document.querySelector(".menu-branding");
 const menu = document.querySelector(".menu");
 const menuNav = document.querySelector(".menu-nav");
 const navItems = document.querySelectorAll(".nav-item");
@@ -12,14 +12,14 @@ menuBtn.addEventListener("click", toggleMenu);
 function toggleMenu() {
   if (!showMenu) {
     menuBtn.classList.add("close");
-    navBranding.classList.add("show");
+    menuBranding.classList.add("show");
     menu.classList.add("show");
     menuNav.classList.add("show");
     navItems.forEach(item => item.classList.add("show"));
     showMenu = true;
   } else {
     menuBtn.classList.remove("close");
-    navBranding.classList.remove("show");
+    menuBranding.classList.remove("show");
     menu.classList.remove("show");
     menuNav.classList.remove("show");
     navItems.forEach(item => item.classList.remove("show"));
